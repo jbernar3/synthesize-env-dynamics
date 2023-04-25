327dc1223325a5a7a1e35b3b570e3d649f677485
cferna12 <84694685+cferna12@users.noreply.github.com>
2023-04-25 13:58:28 -0400

    Update rlang-lamcalc-converter.py

--- rlang-lamcalc-converter.py
+++ rlang-lamcalc-converter.py
@@ -1,1 +1,28 @@
-print("Hello world")
+
+
+def get_lines_method():
+    file_path = "gridworld.rlang"
+    lines = []
+    
+    with open(file_path, 'r') as f:
+        for line in f:
+            # if(printline[0])
+            # print(line[0])
+            words = line.split()
+            if(words):
+                if(words[0] == 'Effect'):
+                    print("effect")
+            # print(words[0])
+            lines.append(words)
+            # print("Current line: ", )
+
+        
+    # print(lines)
+
+def find_end_of_effect():
+    pass
+
+
+# get_sentences()
+get_lines_method()
+
