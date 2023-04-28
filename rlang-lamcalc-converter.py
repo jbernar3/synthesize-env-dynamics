3fa86c36a42d9f05c76306933df55676b6324b40
Jefferson Bernard <jefferson_bernard@brown.edu>
2023-04-28 15:00:58 -0400

    adding if expr parsing

--- rlang-lamcalc-converter.py
+++ rlang-lamcalc-converter.py
@@ -6,7 +6,7 @@ start_chars = ["Effect" , "Proposition", "Factor", "Action", "Constant", "Policy
 
 #Convert file into list of lines of words
 def get_lines_method():
-    file_path = "gridworld.rlang"
+    file_path = "rlang_examples/gridworld.rlang"
     lines = []
     
     with open(file_path, 'r') as f:
@@ -19,7 +19,7 @@ def get_lines_method():
     # print(lines)
     effects = {}
     make_dictionary(lines, effects)
-    print(effects)
+    #print(effects)
 
 
 diction ={"Effect" : {}, "Factor": {}, "Constant": {}, "Proposition" : {}, "Action": {}, "Policy": {}}
@@ -29,7 +29,7 @@ def make_dictionary(lines, effects):
    for idx, line in enumerate(lines):
         if(line[0] in start_chars):
             body = []
-            print(line)
+            #print(line)
             counter = 1
             if(line[2:] != []): #for one line functions
                 body.append(line[3:]) #not great 
@@ -49,7 +49,40 @@ def make_dictionary(lines, effects):
             # effects[line[1].strip(':')] = body 
             diction[line[0]][line[1].strip(':')] =body #Udpate dictionary
 
+# input lines
+# output: "str_output", rest of lines not included in expression
+def translate_one_line(line):
+    return "<one-line-expr>"
+
+def translate_expr(lines):
+    if lines == []:
+        "", []
+    elif lines[0] == []:
+        translate_expr(lines[1:])
+    elif lines[0][0] == 'if':
+        str_out, rst = translate_if_expr(lines)
+        str_out + translate_expr(rst)
+    return "Hello", []
+
+def translate_if_expr(lines):
+    lam_expr = ""
+    if lines == [] or lines[0] == []:
+        return "4", lines
+    elif lines[0][0] == 'if':
+        lam_expr += "(if "
+        lam_expr += translate_one_line(lines[0][1:])
+        out1, rst1 = translate_expr(lines[1:])
+        lam_expr += " " + out1
+        out2, rst2 = translate_expr(rst1)
+        lam_expr += " " + out2
+        return lam_expr + ")", rst2
+    else:
+        return "5", lines
+
 
 get_lines_method()
-print("Dictionary: ", diction)
+print("Effect: ", diction["Effect"]["action_effect"])
+print("---------------------------------")
+out, lines = translate_if_expr(diction["Effect"]["action_effect"])
+print(out)
 # print("effects: ", diction["Effect"]["main"])
