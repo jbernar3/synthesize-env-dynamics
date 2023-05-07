c4fc24826ced08c7f6d1779b5bee3570ca3521bf
Jefferson Bernard <jefferson_bernard@brown.edu>
2023-05-07 16:55:45 -0400

    handle duplicate vars and transform expressions

--- rlang-lamcalc-converter.py
+++ rlang-lamcalc-converter.py
@@ -5,7 +5,7 @@ global_var_num = 0
 
 end_characters = ["#", "Effect" , "Proposition", "Factor", "Action", "Constant", "Policy"]
 start_chars = ["Effect" , "Proposition", "Factor", "Action", "Constant", "Policy"]
-
+operators = ["==", "+", "-"]
 #Convert file into list of lines of words
 def get_lines_method():
     file_path = "rlang_examples/gridworld.rlang"
@@ -51,17 +51,37 @@ def make_dictionary(lines, effects):
             # effects[line[1].strip(':')] = body 
             diction[line[0]][line[1].strip(':')] =body #Udpate dictionary
 
+var_to_num = dict()
+
+def translate_elt(elt):
+    global global_var_num
+    global var_to_num
+    if elt.isnumeric():
+        return elt
+    elif elt in var_to_num:
+        return "$" + var_to_num[elt]
+    elif elt == "[":
+        return "<list>"
+    else:
+        var_to_num[elt] = str(global_var_num)
+        global_var_num += 1
+        return "$" + var_to_num[elt]
+
+
 # input lines
 # output: "str_output", rest of lines not included in expression
 def translate_one_line(line):
+    global operators
     global global_var_num
     if (line[1] == "=="):
+        return "(= " + translate_elt(line[0]) + " " + translate_elt(line[2]) + ")"
+    elif (line[1] in operators):
         first = "$" + str(global_var_num)
         second = "$" + str(global_var_num + 1)
         global_var_num += 2
-        return "(= " + first + " " + second + ")"
+        return "(" + line[1] + " " + translate_elt(line[0]) + " " + translate_elt(line[2]) + ")"
     elif (line[1] == "->"):
-        return "<var-transform-expr>"
+        return translate_one_line(line[2:])
     return "<one-line>"
 
 def translate_expr(lines):
